4d4ad8f1bc474acb0e4e1b3d2090201fd30d6317
GooseJacket <73757413+GooseJacket@users.noreply.github.com>
2024-11-01 20:22:39 -0700

    Update Lab73.java

--- Lab73.java
+++ Lab73.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class LabProgram {
+   //method printOut for printing each line; name, cost, size = padding for formatting, writeSign = for subtotals
    public static void printOut(String name, double cost, int size, boolean writeSign){
       System.out.print(name); //print name
          
@@ -23,15 +24,16 @@ public class LabProgram {
    public static void main(String[] args) {
       //set-up
       Scanner scnr = new Scanner(System.in);
-      double[] costList = {}; String costListString = "";
-      String[] nameList = {}; String nameListString = "";
+      int length = 0;
+      double[] costList = new double[100];
+      String[] nameList = new String[100];
       
       //intro
       System.out.println("Welcome! When inputting costs please put them in normal format but without the dollar sign.");
       System.out.println("Examples of acceptable data: -40.20, 3223, 5.9312, 00.000, -2");
       
       //user input
-      String name = null; String cost = "";
+      String name = null; double cost = 0.0; int index = 0;
       while(scnr.hasNext() && name != ""){
          boolean madeSense = true;
          
@@ -41,47 +43,37 @@ public class LabProgram {
          
          if(name.equals("")){break;} //exit
          
-         
-         
          //Cost
          System.out.print("\nPlease provide the cost of the expense: ");
-         try{cost = scnr.nextLine(); Double.parseDouble(cost);}
+         try{cost = Double.parseDouble(scnr.nextLine());}
          catch(Exception e){
             try{
                System.out.print("\nHey, that didn't make sense. Try the cost again?");
-               cost = scnr.nextLine();
-               Double.parseDouble(cost);
+               cost = Double.parseDouble(scnr.nextLine());
             }
             catch(Exception f){ 
                System.out.print("\nI really don't understand so I'm going to move on.");
                madeSense = false;
          }  }
          if(madeSense){
-            nameListString += name + "SPLITHERE"; //make one long string
-            costListString += cost + "SPLITHERE"; //make one long string
+            nameList[index] = name;
+            costList[index] = cost;
+            index += 1;
+            length += 1;
          }
+         if(length == 100){break;}
       }
       
       System.out.println("\n"); //formatting
       
-      nameList = nameListString.split("SPLITHERE"); //string --> array (because i don't know how many items the user has!)
-      
-      String[] tempList = costListString.split("SPLITHERE"); //string --> array
-      
-      //String[] --> double[]
-      costList = new double[tempList.length]; 
-      for(int i = 0; i < tempList.length; i++){ 
-         costList[i] = Double.parseDouble(tempList[i]);
-      }
-      
       //formatting (basically ensure the dollar signs align)
       int size = "Subtotal:".length();
-      for(String s : nameList){
+      for(int i = 0; i < length; i++){String s = nameList[i];
          if(s.length() > size){size = s.length();}
       }      
       
       double income = 0.0; System.out.println("Income:"); //setup income
-      for(int i = 0; i < costList.length; i++){if(costList[i] >= 0){
+      for(int i = 0; i < length; i++){if(costList[i] >= 0){
          income += costList[i];
          
          printOut(nameList[i], costList[i], size, false); //see top of page
@@ -92,7 +84,7 @@ public class LabProgram {
       System.out.println(); //formatting
       double expenses = 0.0; System.out.println("Expenses:"); //setup expenses
       
-      for(int i = 0; i < costList.length; i++){if(costList[i] < 0){
+      for(int i = 0; i < length; i++){if(costList[i] < 0){
          expenses += costList[i];
          
          printOut(nameList[i], costList[i], size, false); //see top of page
